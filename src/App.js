abc5a735f95770029af3741cea964c0a35f1c7a8
Archit Warghane <archit2x3@hotmail.com>
2020-03-01 18:18:20 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+const ExpenseDashboardPage = () => <div> ExpenseDashboardPage </div>;
+const AddExpensePage = () => <div>AddExpensePage</div>;
+const EditExpensePage = () => <div>EditExpense</div>;
+const HelpPage = () => <div>Help</div>;
+const PageNotFound = () => (
+	<div style={{ textAlign: 'center' }}>
+		<h1>404, LoL</h1>
+	</div>
+);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" component={ExpenseDashboardPage} exact={true} />
+				<Route path="/create" component={AddExpensePage} />
+				<Route path="/edit" component={EditExpensePage} />
+				<Route path="/help" component={HelpPage} />
+				<Route component={PageNotFound} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
